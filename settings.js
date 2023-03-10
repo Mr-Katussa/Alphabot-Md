bc68ded003677eacd079fbb52e0c7ecbec63d52b
MR Katussa <108258060+Mr-Katussa@users.noreply.github.com>
2023-03-10 16:29:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -41,17 +41,17 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
+global.botname = "Alphabot-Md" //namabot kalian
+global.ownername = "Sandaru" //nama kalian
+global.myweb = "https://s.id/Sandaru" //bebas asal jan hapus
+global.youtube = "https://youtube.com/SANDARU-YT" //bebas asal jan hapus
+global.myweb2 = "https://s.id/sandaru" //bebas
+global.email = "mdsandaru2@gmail.com" //bebas
+global.region = "Sri Lanka" //bebas
+global.timezone = 'Asia/Colombo' //  timezone wib
+global.premium = ['15517868003'] //premium user
+global.owner = ["15517868003", "447710173736"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "94764497078" // nomor wa kalian
 global.ownernomerr = "+"+ownernomer //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
@@ -81,17 +81,17 @@ global.buttonDefault = [{
    }
 }, {
    quickReplyButton: {
-      displayText: '🏓 Ping',
+      displayText: 'SPEED',
       id: '.ping'
    }
 }, {
    quickReplyButton: {
-      displayText: 'Owner 👤',
+      displayText: 'OWNER',
       id: '.owner'
    }
 }, {
    quickReplyButton: {
-      displayText: '⚡ Command ⚡',
+      displayText: 'MENU',
       id: '.command'
    }
 }]
@@ -107,24 +107,24 @@ global.buttonDefault2 = [{
    }
 }, {
    quickReplyButton: {
-      displayText: '🏓 Ping',
+      displayText: 'SPEED',
       id: '.ping'
    }
 }, {
    quickReplyButton: {
-      displayText: 'Owner 👤',
+      displayText: 'OWNER',
       id: '.owner'
    }
 }, {
    quickReplyButton: {
-      displayText: '🎁 Donasi 🎁',
+      displayText: 'DONATE',
       id: '.donasi'
    }
 }]
 global.buttonDefault3 = [{
       buttonId: `.owner`,
       buttonText: {
-         displayText: '🙍‍♂️ Owner'
+         displayText: 'OWNER'
       },
       type: 1
    },
